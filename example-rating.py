ac5fcc94a3809ac583017fa06f8d06f793c67300
Pavel Bocharov <pavelbocharov@gmail.com>
2017-12-12 23:20:03 +0300

    Added comments

--- example-rating.py
+++ example-rating.py
@@ -16,28 +16,36 @@ def average_payoff(pm, scoring_func):
     df = pd.DataFrame(game.matrix_apply_scoring(pm, scoring_func))
     return (-df.sum() / (len(df.columns) - 1)).values
 
-n, m = 25, 5
+n, m = 16, 6
 partitions = core.all_partitions(n, m)
 df = pd.DataFrame(partitions)
 
+# Load or calculate a payoff matrix for all partitions
 try:
     pm = data.load_payoff_matrix_lotto_permute(n, m)
 except FileNotFoundError:
     pm = game.payoff_matrix_lotto_permute(partitions)
 
+# Calculate and add to the dataframe average payoff using a specified
+# scoring function
 df = df.assign(avg_permute=average_payoff(
     pm,
     game.scoring_sign))
+
+# Calculate and add to the dataframe average payoff for the different type of
+# payoff function based on partition resource function
 df = df.assign(avg_resource=average_payoff(
     game.payoff_matrix_zero_sum(partitions, game.payoff_lotto_resource),
     game.scoring_sign))
 
+# Calculate and add to the dataframe average payoff for the game between
+# partition families
 adjacency_m = core.family_adjacency_matrix(partitions, branches=[-1, 0, 1])
 family_pm = game.payoff_matrix_graph(pm, adjacency_m)
 df = df.assign(family_avg=average_payoff(
     family_pm,
     game.scoring_sign))
 
-# Print all data frame
+# Print the whole dataframe
 for t in df.itertuples():
     print("\t".join(map(str, t)))
